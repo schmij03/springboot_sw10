723aa0f7dd4a682f5cc2d0fb809feac25ed06e3d
schmij03 <96172389+schmij03@users.noreply.github.com>
2023-05-29 17:57:15 +0200

    first commit

---
+++ src/test/java/com/schmij03/sw9/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schmij03.sw9.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
